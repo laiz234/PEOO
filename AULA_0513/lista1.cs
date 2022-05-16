5ef5b65252f0b6676cbab8370a7d3c3029ca5194
laizfernanda <82954279+laiz234@users.noreply.github.com>
2022-05-16 18:27:39 +0000

    adicionei a pasta da lista 1

---
+++ AULA_0513/lista1.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Program{
+  public static void Main(string[] args){
+    Console.WriteLine("Laiz Fernanda Souza dos Santos");
+    Console.WriteLine("20201011110032");
+  }  
+}
